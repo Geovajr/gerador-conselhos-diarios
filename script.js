878fc8eb3bb74a73f114dff895fdde60b290ee1b
geova <geova.dev@gmail.com>
2025-09-24 21:21:38 -0300

    Criação e ipload do README.md e dos demais arquivos

---
+++ script.js
@@ -0,0 +1,9 @@
+const conselhos = [
+    "Acredite em voçe mesmo!",
+    " O Primeiro Passo é o mais importante!",
+    "A persistência realiza o impossível!",
+    "Aprenda algo novo hoje!",
+    "Seja gentil com todos que encontrar!"
+];
+
+console.log("Arquivo de script carregado. Array de conselhos pronto");
